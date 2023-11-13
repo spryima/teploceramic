4737fa18b60f5200b8e6b7e2af12fc5fad633077
SergiiPryima <25337789-SergiiPryima@users.noreply.replit.com>
2023-11-13 18:08:31 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+import requests
+
+from xml.etree import ElementTree as ET
+
+url = 'https://teploceramic.com.ua/tstore/yml/0c27a1c7f14adcdbcc9b5fc249bdb410.yml'
+response = requests.get(url)
+xml_data = response.content
+
+tree = ET.ElementTree(ET.fromstring(xml_data))
+root = tree.getroot()
+
+for offer in root.findall('.//offer'):
+  offer.set('available', 'true')
+
+tree.write('modified_file.xml')
+os.system('git add modified_file.xml')
+os.system('git commit -m "Оновлено файл"')
+os.system('git push')
